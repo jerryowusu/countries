ee9fb506952f0a8a641d6a2c3db2595ccc084050
jerryowusu <jerryowusu50@gmail.com>
2023-03-28 14:18:31 +0000

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-orange-500 underline text-center">Hello Jerry!</h1>
+    </div>
+  )
+}
+
+export default App
